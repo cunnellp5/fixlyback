adf52c8039fc0da36f71bc97cc897fc37d9963e6
cunnellp5 <philip.cunnell@colorado.edu>
2017-05-31 13:21:39 -0600

    added lat lng and time to db

--- migrations/20170523135939_help.js
+++ migrations/20170523135939_help.js
@@ -2,6 +2,10 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('help', table => {
     table.increments();
     table.text('description').notNullable();
+    table.string('day');
+    table.string('time');
+    table.float('lat', 6);
+    table.float('lng', 6);
     table.enu('request', ['active', 'pending', 'completed']);
     table.integer('user_id').references('user.id').unsigned().onDelete('cascade');
   });
--- seeds/02_helps.js
+++ seeds/02_helps.js
@@ -1,13 +1,23 @@
+const moment = require('moment');
+
 exports.seed = (knex, Promise) => {
   return knex.raw('DELETE FROM help; ALTER SEQUENCE help_id_seq RESTART WITH 1')
       .then(() => {
         const help = [{
           description: 'bike chain broken',
           request: 'active',
+          lat: 39.7392,
+          lng: -104.9903,
+          time: moment(Date.now()).format('h:mm:ss a'),
+          day: moment(Date.now()).format('MMMM Do YYYY'),
           user_id: 1
         }, {
           description: 'bike tire popped',
           request: 'completed',
+          lat: 39.7392,
+          lng: -104.9903,
+          time: moment(Date.now()).format('h:mm:ss a'),
+          day: moment(Date.now()).format('MMMM Do YYYY'),
           user_id: 1
         }];
         return knex('help').insert(help);
